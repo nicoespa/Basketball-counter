63895d517906d9a1da2bfe83811fbeee460d11ce
nicoespa <nicolasspagnuolo4@gmail.com>
2025-01-18 12:55:30 -0300

    Commit initial files

---
+++ Scrim-s04k019/index.js
@@ -0,0 +1,38 @@
+let resultH = document.getElementById("resultH")
+let resultG = document.getElementById("resultG")
+resultadoH = 0
+resultadoG = 0
+
+
+function oneh(){
+    resultadoH += 1
+   resultH.textContent = resultadoH
+}
+
+function twoh(){
+    resultadoH += 2
+   resultH.textContent = resultadoH
+}
+
+function threeh(){
+    resultadoH += 3
+   resultH.textContent = resultadoH
+}
+
+
+function oneg(){
+    resultadoG += 1
+   resultG.textContent = resultadoG
+}
+
+function twog(){
+    resultadoG += 2
+   resultG.textContent = resultadoG
+}
+
+function threeg(){
+    resultadoG += 3
+   resultG.textContent = resultadoG
+}
+
+
